737b74225d4d0afdce57d7b48b0dc205b175d67a
MTrucky <146337304+MTrucky@users.noreply.github.com>
2024-03-26 17:24:50 +0500

    Create linux.py

---
+++ linux.py
@@ -0,0 +1,21 @@
+def read_file_with_limit():
+    with open('/home/maslenok/Рабочий стол/text.txt', 'r') as file:
+        for line in file:
+            if len(line) > 20:
+                yield line[:20]
+            else:
+                yield line
+
+def reverse_words(line):
+    words = line.split()
+    reversed_words = []
+    for word in reversed(words):
+        reversed_words.append(word)
+    return ' '.join(reversed_words)
+
+lines_with_limit = read_file_with_limit()
+
+reversed_lines = map(reverse_words, lines_with_limit)
+
+for line in reversed_lines:
+    print(line)
